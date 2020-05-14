6c19638df1a20cc33751f6fbe51094cf92c55030
root <root@0a5f5f4d495f>
2020-05-14 03:00:52 +0000

    Nasty Suprise(IP)

---
+++ src/_01_nasty_surprise/NastySuprise.java
@@ -0,0 +1,16 @@
+package _01_nasty_surprise;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class NastySuprise {
+JFrame j = new JFrame();
+JPanel p = new JPanel();
+JButton b1 = new JButton();
+JButton b2 = new JButton();
+JLabel l1 = new JLabel();
+JLabel l2 = new JLabel();
+
+}
