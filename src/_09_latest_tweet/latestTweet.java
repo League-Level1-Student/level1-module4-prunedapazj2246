9369569552c300e41620262e376a5c15a1fca578
root <root@0715bc7e7dd8>
2020-07-23 02:58:22 +0000

    Finished latest Tweet Pig Latin Translator(IP)

--- src/_09_latest_tweet/latestTweet.java
+++ src/_09_latest_tweet/latestTweet.java
@@ -8,6 +8,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
 public class latestTweet implements MouseListener {
 	JFrame f = new JFrame();
 			JPanel p = new JPanel();
@@ -42,11 +48,33 @@ public void mouseExited(MouseEvent e) {
 @Override
 public void mousePressed(MouseEvent e) {
 	// TODO Auto-generated method stub
-	
+System.out.println("Tweet Tweet");
+System.out.println(getLatestTweet(String.valueOf(jtf.getText())));
+
 }
 @Override
 public void mouseReleased(MouseEvent e) {
 	// TODO Auto-generated method stub
 	
+}
+private String getLatestTweet(String searchingFor) {
+
+    Twitter twitter = new TwitterFactory().getInstance();
+
+    AccessToken accessToken = new AccessToken(
+        "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+        "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+        "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+    twitter.setOAuthAccessToken(accessToken);
+
+Query query = new Query(searchingFor);
+    try {
+        QueryResult result = twitter.search(query);
+        return result.getTweets().get(0).getText();
+    } catch (Exception e) {
+        System.err.print(e.getMessage());
+        return "What the heck is that?";
+    }
 }
 }
---
+++ src/_10_pig_latin/PigLatinTranslator.java
@@ -0,0 +1,117 @@
+package _10_pig_latin;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PigLatinTranslator implements MouseListener {
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
+	JTextField pigLatin = new JTextField(25);
+	JButton translate = new JButton();
+	JTextField english = new JTextField(25);
+	public void run() {
+		translate.setText("translate");
+		translate.addMouseListener(this);
+		p.add(pigLatin);
+		p.add(translate);
+		p.add(english);
+		f.add(p);
+		f.setTitle("Joaquin's Pig Latin Translator");
+		f.setVisible(true);
+		f.pack();
+	}
+	/**
+	* Method to test whether a character is a letter or not.
+	* @param c The character to test
+	* @return True if it's a letter
+	*/
+	private static boolean isLetter(char c) {
+	return ( (c >= 'A' && c <= 'Z') || (c >='a' && c <= 'z') );
+	}
+	    
+	/**
+	* Method to translate one word into pig latin.
+	* @param word The word in english
+	* @return The pig latin version
+	*/
+	private static String pigWord(String word) {
+	int split = firstVowel(word);
+	return word.substring(split)+"-"+word.substring(0, split)+"ay";
+	}
+		
+	/**
+	* Method to translate a sentence word by word.
+	* @param s The sentence in English
+	* @return The pig latin version
+	*/
+	public String translate(String s) {
+	String latin = "";
+	int i = 0;
+	while (i < s.length()) {
+	// Take care of punctuation and spaces
+	while (i < s.length() && !isLetter(s.charAt(i))) {
+	latin = latin + s.charAt(i);
+	i++;
+	}
+	// If there aren't any words left, stop.
+	if (i>=s.length()) break;
+	// Otherwise we're at the beginning of a word.
+	int begin = i;
+	while (i < s.length() && isLetter(s.charAt(i))) {
+	i++;
+	}
+	// Now we're at the end of a word, so translate it.
+	int end = i;
+	latin = latin + pigWord(s.substring(begin, end));
+	}
+	return latin;
+	}
+	/**
+	* Method to find the index of the first vowel in a word.
+	* @param word The word to search
+	* @return The index of the first vowel
+	*/
+		  private static int firstVowel(String word) {
+	word = word.toLowerCase();
+	for (int i=0; i < word.length(); i++)
+	if (word.charAt(i)=='a' || word.charAt(i)=='e' ||
+	word.charAt(i)=='i' || word.charAt(i)=='o' ||
+	word.charAt(i)=='u')
+	return i;
+	return 0;
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		isLetter();
+		pigWord(String.valueOf(pigLatin.getText()));
+		translate();
+		firstVowel();
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	}
+
